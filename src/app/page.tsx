e3f30401c518b693ff769667f78b0121dee15c43
Alejandro Fernández Valdivia <alejandrofnv@gmail.com>
2025-03-16 17:03:05 +0100

    feat: uncomment portfolio sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
-// import About from "@/components/about"
-import Construction from "@/components/construction"
-// import Contact from "@/components/contact"
-// import Experience from "@/components/experience"
-// import Footer from "@/components/footer"
-// import Header from "@/components/header"
-// import Hero from "@/components/hero"
+import About from "@/components/about"
+// import Construction from "@/components/construction"
+import Contact from "@/components/contact"
+import Experience from "@/components/experience"
+import Footer from "@/components/footer"
+import Header from "@/components/header"
+import Hero from "@/components/hero"
 // import Projects from "@/components/projects"
-// import Skills from "@/components/skills"
+import Skills from "@/components/skills"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-background">
-      <Construction />
-      {/* <Header /> */}
-      {/* <Hero /> */}
-      {/* <About /> */}
-      {/* <Skills /> */}
-      {/* <Experience /> */}
+      {/* <Construction /> */}
+      <Header />
+      <Hero />
+      <About />
+      <Skills />
+      <Experience />
       {/* <Projects /> */}
-      {/* <Contact /> */}
-      {/* <Footer /> */}
+      <Contact />
+      <Footer />
     </main>
   )
 }
